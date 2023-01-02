4f9ba6a3d0d1b11202677bc704d1ab411cc5c872
bozdoz <howaboutben@gmail.com>
2023-01-02 18:14:16 +0000

    Day 22, Part 1

---
+++ 22/day-22.go
@@ -0,0 +1,39 @@
+package main
+
+import "github.com/bozdoz/advent-of-code-2022/utils"
+
+// today's input data type
+type inType = []string
+
+// how to read today's input
+var fileReader = utils.ReadEmptyLineGroups
+
+// today's output data type
+type outType = int
+
+func partOne(data inType) (ans outType) {
+	board := parseInput(data)
+
+	state := start(board)
+
+	return 1000*(state.row+1) + 4*(state.col+1) + int(state.face)
+}
+
+func partTwo(data inType) (ans outType) {
+	return
+}
+
+//
+// BOILERPLATE BELOW
+//
+
+func main() {
+	// pass file reader and functions to call with input data
+	utils.RunSolvers(utils.Day[inType, outType]{
+		FileReader: fileReader,
+		Fncs: []func(inType) outType{
+			partOne,
+			partTwo,
+		},
+	})
+}
