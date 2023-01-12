fed8d1cede4cea94bba99a3bf349149cf5609564
bozdoz <howaboutben@gmail.com>
2023-01-12 04:04:04 +0000

    failed bfs day 19

--- 19/day-19.go
+++ 19/day-19.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/bozdoz/advent-of-code-2022/utils"
+import (
+	"fmt"
+
+	"github.com/bozdoz/advent-of-code-2022/utils"
+)
 
 // today's input data type
 type inType = []string
@@ -15,13 +19,32 @@ func partOne(data inType) (ans outType) {
 	blueprints := parseInput(data)
 
 	for i, bp := range blueprints {
-		ans += bp.bestPath(24) * (i + 1)
+		best := bp.bestPath(24)
+		ans += best * (i + 1)
+
+		// example should be 9 and 12
+		fmt.Println("best of all:", best)
 	}
 
+	// acutal input should be 1144
 	return
 }
 
 func partTwo(data inType) (ans outType) {
+	blueprints := parseInput(data)
+
+	for i, bp := range blueprints {
+		if i > 1 {
+			break
+		}
+		best := bp.bestPath(32) * (i + 1)
+
+		// example should be 56 and 62
+		fmt.Println("best for", i+1, ":", best)
+
+		ans += best
+	}
+
 	return
 }
 
